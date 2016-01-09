8c28c24f8dd597cd08297b8c13a9fdd848e46e01
Heiner Peuser <heiner.peuser@gmail.com>
2016-01-09 13:21:08 +0100

    Added REST Service /rs/ping to check for session validity

--- core/src/main/java/net/weweave/tubewarder/service/model/ErrorCode.java
+++ core/src/main/java/net/weweave/tubewarder/service/model/ErrorCode.java
@@ -5,4 +5,5 @@ public class ErrorCode {
     public static final int INVALID_INPUT_PARAMETERS = 1;
     public static final int OBJECT_LOOKUP_ERROR = 2;
     public static final int PERMISSION_DENIED = 3;
+    public static final int AUTH_REQUIRED = 4;
 }
---
+++ core/src/main/java/net/weweave/tubewarder/service/rest/PingService.java
@@ -0,0 +1,37 @@
+package net.weweave.tubewarder.service.rest;
+
+import net.weweave.tubewarder.domain.Session;
+import net.weweave.tubewarder.exception.PermissionException;
+import net.weweave.tubewarder.service.model.ErrorCode;
+import net.weweave.tubewarder.service.request.AbstractRestRequest;
+import net.weweave.tubewarder.service.response.AbstractResponse;
+
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@RequestScoped
+@Path("/ping")
+public class PingService extends AbstractService {
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(JaxApplication.APPLICATION_JSON_UTF8)
+    public AbstractResponse action(AbstractRestRequest request) {
+        AbstractResponse response = new AbstractResponse();
+
+        try {
+            loadSession(request.token);
+        } catch (Exception e) {
+            response.error = ErrorCode.INVALID_INPUT_PARAMETERS;
+        }
+
+        return response;
+    }
+
+    private void loadSession(String token) throws PermissionException {
+        Session session = getSession(token);
+    }
+}
