eb257f83cdefc63830b28aa161f32a43094e33f1
Heiner Peuser <heiner.peuser@gmail.com>
2016-01-15 11:58:06 +0100

    Refactored TestPingService

--- core/src/test/java/net/weweave/tubewarder/test/rest/TestPingService.java
+++ core/src/test/java/net/weweave/tubewarder/test/rest/TestPingService.java
@@ -1,8 +1,7 @@
 package net.weweave.tubewarder.test.rest;
 
-import com.jayway.restassured.http.ContentType;
+import com.jayway.restassured.specification.ResponseSpecification;
 import net.weweave.tubewarder.service.model.ErrorCode;
-import org.apache.http.HttpStatus;
 import org.jboss.arquillian.junit.Arquillian;
 import org.json.JSONObject;
 import org.junit.Test;
@@ -10,62 +9,40 @@ import org.junit.runner.RunWith;
 
 import java.util.UUID;
 
-import static com.jayway.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.equalTo;
 
 @RunWith(Arquillian.class)
 public class TestPingService extends AbstractRestTest {
     @Test
     public void testInvalidToken() {
-        JSONObject payload = new JSONObject();
-        payload.put("token", UUID.randomUUID().toString());
-
-        given()
-                .body(payload.toString())
-                .contentType(ContentType.JSON)
-        .expect()
-                .contentType(ContentType.JSON)
-                .statusCode(HttpStatus.SC_OK)
-                .body("error", equalTo(ErrorCode.INVALID_INPUT_PARAMETERS))
-        .when()
-                .post(getUri("ping"))
-                .asString();
+        validatePingResponse(UUID.randomUUID().toString(),
+                "error", equalTo(ErrorCode.INVALID_INPUT_PARAMETERS));
     }
 
     @Test
     public void testEmptyToken() {
-        JSONObject payload = new JSONObject();
-        payload.put("token", "");
-
-        given()
-                .body(payload.toString())
-                .contentType(ContentType.JSON)
-        .expect()
-                .contentType(ContentType.JSON)
-                .statusCode(HttpStatus.SC_OK)
-                .body("error", equalTo(ErrorCode.INVALID_INPUT_PARAMETERS))
-        .when()
-                .post(getUri("ping"))
-                .asString();
+        validatePingResponse("",
+                "error", equalTo(ErrorCode.INVALID_INPUT_PARAMETERS));
     }
 
     @Test
     public void testValidToken() {
         createAdminUser();
         String token = authAdminGetToken();
+        validatePingResponse(token,
+                "error", equalTo(ErrorCode.OK));
+    }
 
+    private JSONObject validatePingResponse(String token, Object... body) {
+        JSONObject payload = getPingRequestPayload(token);
+        ResponseSpecification response = getResponseSpecificationPost(payload);
+        setExpectedBodies(response, body);
+        return getPostResponse(response, "ping");
+    }
+
+    private JSONObject getPingRequestPayload(String token) {
         JSONObject payload = new JSONObject();
         payload.put("token", token);
-
-        given()
-                .body(payload.toString())
-                .contentType(ContentType.JSON)
-        .expect()
-                .contentType(ContentType.JSON)
-                .statusCode(HttpStatus.SC_OK)
-                .body("error", equalTo(ErrorCode.OK))
-        .when()
-                .post(getUri("ping"))
-                .asString();
+        return payload;
     }
 }
