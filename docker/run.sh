4d0c1930fa4e04d60fbdcf4d4190e81e1954e575
Heiner Peuser <heiner.peuser@gmail.com>
2016-01-20 22:57:27 +0100

    Added ability to build docker image from maven

---
+++ docker/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ ! -d /var/lib/mysql/tubewarder ];
+then
+    echo "Initializing tubewarder database..."
+    /usr/bin/mysqld_safe > /dev/null 2>&1 &
+    RET=1
+    while [[ RET -ne 0 ]]; do
+        sleep 5
+        mysql -uroot -e "status" > /dev/null 2>&1
+        RET=$?
+    done
+    mysql -uroot -e "CREATE DATABASE IF NOT EXISTS tubewarder;"
+    mysql -uroot -e "GRANT USAGE ON *.* TO 'tubewarder'@'localhost' IDENTIFIED BY 'tubewarder';"
+    mysql -uroot -e "GRANT ALL PRIVILEGES ON tubewarder.* TO 'tubewarder'@'localhost';"
+    mysqladmin -uroot shutdown
+fi
+
+exec /usr/bin/supervisord -n
