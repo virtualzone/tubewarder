a11a2fedd9400dfe762ff4f7f93ff09ba19be131
Heiner Peuser <heiner.peuser@gmail.com>
2016-01-21 21:53:24 +0100

    Fixed issue: docker image not created database on initial start

--- docker/run.sh
+++ docker/run.sh
@@ -3,6 +3,8 @@
 if [ ! -d /var/lib/mysql/tubewarder ];
 then
     echo "Initializing tubewarder database..."
+    chown -R mysql:mysql /var/lib/mysql
+    /usr/bin/mysql_install_db
     /usr/bin/mysqld_safe > /dev/null 2>&1 &
     RET=1
     while [[ RET -ne 0 ]]; do
